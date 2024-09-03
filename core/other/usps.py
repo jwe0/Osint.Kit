764d7bf1a12bf999f1dd824b06221ea6febd70c4
jwe0 <jwe0@epic.x>
2024-09-03 14:08:31 +0100

    Updated usps module

--- core/other/usps.py
+++ core/other/usps.py
@@ -1,15 +1,23 @@
 import tls_client
 def USPSLookup(args):
     session = tls_client.Session()
-    data = {}
+    log = {}
     address = args.get("code", "")
-
     if not address:
         return {"message" : "error", "info" : "You did not supply address information"}
     api = "https://tools.usps.com/tools/app/ziplookup/cityByZip"
     data = {"zip" : address}
     r = session.post(api, data=data)
     if r.status_code == 200:
-        return {"message" : "success", "info" : r.json()}
+        decode = r.json()
+        log = {
+            "resultStatus" : decode.get("resultStatus"),
+            "zip5" : decode.get("zip5"),
+            "defaultCity" : decode.get("defaultCity"),
+            "defaultState" : decode.get("defaultState"),
+            "defaultRecordType" : decode.get("defaultRecordType"),
+            "nonAcceptList" : ", ".join(f"{user.get('city')} : {user.get('state')}" for user in decode.get("nonAcceptList"))
+        }
+        return {"message" : "success", "info" : log}
     else:
         return {"message" : "error", "info" : f"{str(r.status_code)} : {r.text if r.text else 'Unknown error'}"}
